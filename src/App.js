4e0eacd254d579d20772dd02ed902265d60dbe6e
Hans-0101 <hyhsumin@naver.com>
2022-01-17 15:48:15 +0900

    feat:initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import ChatBot from 'react-simple-chatbot';
+import ChatForm from './components/ChatForm';
+import './index.css';
+
+
+function App() {
+  
+  return (
+    <div>
+      <ChatForm/>
+    </div>
+  );
+}
+export default App;
